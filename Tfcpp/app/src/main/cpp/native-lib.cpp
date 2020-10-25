1890653475039341b25f08633fa3903d9235cfda
Ayyuce Demirbas <ayyucedemirbas@windowslive.com>
2020-10-25 20:57:07 +0300

    it works

---
+++ Tfcpp/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,39 @@
+#include <jni.h>
+#include <string>
+#include <iostream>
+#include "../jni/common.h"
+#include "../jni/builtin_ops.h"
+#include "../jni/c_api.h"
+
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_tfcpp_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    const char *path = "/home/ayyuce/Desktop/model.tflite";
+    TfLiteModel * model = TfLiteModelCreateFromFile(path);
+    /*TfLiteInterpreter * interpreter = TfLiteInterpreterCreate(model);
+    TfLiteInterpreterAllocateTensors(interpreter);
+    TfLiteTensor * input_tensor =
+            TfLiteInterpreterGetInputTensor(interpreter, 0);
+    const TfLiteTensor * output_tensor =
+            TfLiteInterpreterGetOutputTensor(interpreter, 0);
+    TfLiteStatus from_status = TfLiteTensorCopyFromBuffer(
+            input_tensor,
+            input_data,
+            TfLiteTensorByteSize(input_tensor));
+    TfLiteStatus interpreter_invoke_status = TfLiteInterpreterInvoke(interpreter);
+    TfLiteStatus to_status = TfLiteTensorCopyToBuffer(
+            output_tensor,
+            output_data,
+            TfLiteTensorByteSize(output_tensor));*/
+    auto version = TfLiteVersion();
+    std::string s(version);
+    std::string hello= version;
+    //TfLiteModelCreateFromFile()
+
+    //TfLiteVersion();
+    //std::string hello = "Hello from C++";
+
+    return env->NewStringUTF(hello.c_str());
+}
